6f335ab20d6ff056ee303ace7061ccc85ad04e47
MoonJink <moonbotjoe@gmail.com>
2024-10-18 20:50:58 +0100

    Initial commit

---
+++ creature_random_gen.py
@@ -0,0 +1,112 @@
+# ------------------------------
+# IMPORTS
+# ------------------------------
+import random
+
+# ------------------------------
+# SUBPROGRAMS
+# ------------------------------
+
+def species_type():
+    species_type_array = [
+        "Cursed",
+        "Transformed",
+        "Divine",
+        "Monster",
+        "Beast"
+    ]
+    return random.choice(species_type_array)
+
+def environment():
+    environment_array = [
+        "Aquatic",
+        "Semi-Aquatic",
+        "Land",
+        "Flying"
+    ]
+    return random.choice(environment_array)
+
+def country():
+    country_array = [
+        "Silva",
+        "Yever",
+        "Ashborne",
+        "TROK",
+        "Breunswick",
+        "Thornsdale",
+        "Wranbourne",
+        "Sanguis (UPPER)",
+        "Sanguis (LOWER)",
+        "Formellond",
+        "Imperium",
+        "Lindow Tribes",
+        "Volcano",
+        "Libertas",
+        "South Libertas",
+        "Vindium",
+        "Aranae",
+        "Whimsy",
+    ]
+    chosen_country = random.choice(country_array)
+
+    match chosen_country:
+        case "Sanguis (UPPER)" | "Sanguis (LOWER)":
+            sanguis_sub_biome = [
+                " (cold)",
+                " (taiga)",
+                " (tundra)"
+            ]
+            chosen_sub_biome = random.choice(sanguis_sub_biome)
+            chosen_country = chosen_country + chosen_sub_biome
+            return chosen_country
+        case "Ashborne":
+            ashborne_sub_biome = [
+                " (desert)",
+                " (savanna)"
+            ]
+            chosen_sub_biome = random.choice(ashborne_sub_biome)
+            chosen_country = chosen_country + chosen_sub_biome
+            return chosen_country
+        case "Silva":
+            silva_sub_biome = [
+                " (greater island)",
+                " (unknown island)",
+                " (lesser island)"
+            ]
+            chosen_sub_biome = random.choice(silva_sub_biome)
+            chosen_country = chosen_country + chosen_sub_biome
+            return chosen_country
+        case _:
+            return chosen_country
+
+def aggression():
+    aggression_array = [
+        "Low",
+        "Medium",
+        "High"
+    ]
+    return random.choice(aggression_array)
+
+def danger():
+    danger_array = [
+        "Low",
+        "Medium",
+        "High"
+    ]
+    return random.choice(danger_array)
+
+def diet():
+    diet_array = [
+        "Carnivore",
+        "Herbivore",
+        "Omnivore",
+        "Other"
+    ]
+    return random.choice(diet_array)
+
+# ------------------------------
+# MAIN
+# ------------------------------
+def main():
+    print(f"\n'{species_type()}' from '{country()}' in a '{environment()}' habitat with aggression level '{aggression()}' and danger level '{danger()}'. Its diet is '{diet()}'.")
+main()
